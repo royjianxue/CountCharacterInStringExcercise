2bf40c9e595bb4a996520606a74757385a21059b
Jian Xue <royjianxue@yahoo.com>
2022-05-27 09:55:33 -0400

    Add project files.

---
+++ CountCharacterInString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharacterInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata.Count("Hello").ToList().ForEach(x => Console.WriteLine($"{x.Key} : {x.Value}"));
+
+            Console.ReadLine();
+        }
+    }
+    public class Kata
+    {
+        public static Dictionary<char, int> Count(string str)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char c in str)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    dic[c] += 1;
+                }
+                else
+                {
+                    dic.Add(c, 1);
+                }
+            }
+            return dic;
+
+        }
+    }
+}
